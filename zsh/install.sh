a2fa268df2209093bfc9a078189ae0a3626ee006
Borys Nebosenko <boris.nebosenko@gmail.com>
2020-05-04 10:07:39 -0400

    Minor fixes for zsh installation

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Install zsh (need sudo access)..."
+sudo apt install zsh
+
 echo "Configuring zsh..."
 
 zshrc_dir=$(dirname $(realpath $BASH_SOURCE))
@@ -7,7 +10,7 @@ zshrc_file=".zshrc"
 
 change=". ${zshrc_dir}/${zshrc_file}"
 
-if [ ! -f "${HOME}/.oh-my-zsh" ]; then
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
     echo "Install Oh-My-Zsh..."
     echo "When installation completes exit zsh by typing exit<Return>..."
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -15,7 +18,7 @@ fi
 
 powerlevel10k_dir=${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k
 
-if [ ! -f "${powerlevel10k_dir}" ]; then
+if [ ! -d "${powerlevel10k_dir}" ]; then
     echo "Install Powerlevel10k..."
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${powerlevel10k_dir}
 else
@@ -24,6 +27,7 @@ fi
 
 cp ${zshrc_dir}/.p10k.zsh ${HOME}/
 
+touch ~/${zshrc_file}
 grep -q "Configuration provided by nbu/linux-settings" ~/${zshrc_file}
 res=$?
 
