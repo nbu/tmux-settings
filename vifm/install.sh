af504babd6aefae2d363b996ef6ed9fe380d5596
Borys Nebosenko <boris.nebosenko@gmail.com>
2020-05-04 17:31:16 -0400

    Fixed vifm installation

--- vifm/install.sh
+++ vifm/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 custom_conf_dir=$(dirname $(realpath $BASH_SOURCE))
+origin_conf_dir="${HOME}/.config/vifm"
 conf_file="vifmrc"
 
 echo "Install VIFM (need sudo access)..."
 sudo apt install vifm
 
 echo "Configuring VIFM"
-mv ~/${conf_file} ~/${conf_file}.back
+mv ${origin_conf_dir}/${conf_file} ${origin_conf_dir}/${conf_file}.back
 touch ~/${confi_file}
-echo "source ${custom_conf_dir}/${conf_file}" > ~/${conf_file}
+echo "source ${custom_conf_dir}/${conf_file}" > ${origin_conf_dir}/${conf_file}
 
