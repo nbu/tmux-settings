d9cd915b2af9fe8c224fe60ff96af27e5f50e2f6
Borys Nebosenko <boris.nebosenko@gmail.com>
2020-05-03 23:25:22 -0400

    Minor fixes

--- zsh/install.sh
+++ zsh/install.sh
@@ -11,13 +11,12 @@ echo "Install Oh-My-Zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Install Powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 set -e
 grep -q "Configuration provided by nbu/linux-settings" ~/${zshrc_file}
 res=$?
 set +e
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 if [ ${res} -eq 1 ]; then
     echo >> ~/${zshrc_file}
