179bf9ddbabcd9319bf0fa6ceac364f592116acf
Borys Nebosenko <bnebosenko@intralinks.com>
2020-06-29 23:32:13 -0400

    Add run swagger in docker alias. Add tools installation on osx

--- shared/install.sh
+++ shared/install.sh
@@ -7,10 +7,16 @@ pip install pyyaml
 
 isUbuntu
 ubuntu=$?
+isOSX
+osx=$?
+
 
 if [ $ubuntu -eq 0 ]; then
     echo "Install ncdu, htop, tldr, jq (need sudo access)..."
     sudo apt install ncdu htop tldr jq
+elif [ $osx -eq 0 ]; then
+    echo "Install ncdu, htp, ltdr, jq..."
+    brew install ncdu htop tldr jq
 else
     echo "Warning, OS is not supported. Be sure zsh is installed"
 fi
